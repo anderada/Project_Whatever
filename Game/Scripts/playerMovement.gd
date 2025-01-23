9c8c3c34d4900665dc8b3c00fc35b7e213e80194
Adam Anderson <anderada@sheridancollege.ca>
2025-01-23 22:25:42 +0000

    ice sliding and colisions

--- Game/Scripts/playerMovement.gd
+++ Game/Scripts/playerMovement.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var moveCurve : Curve
 @export var rotationTime : float
 @export var rotationCurve : Curve
+@export var mazeRef : GridMap
 var forward : Vector2
 var nextSpace : Vector2
 var targetPosition : Vector3
@@ -14,6 +15,7 @@ var moveClock : float = 0
 var rotationClock : float = 0
 var lastMove : Vector2
 var playerInput = Vector2(0,0)
+var sliding = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,42 +24,26 @@ func _ready() -> void:
 	targetRotation = rotation_degrees.y
 	previousRotation = rotation_degrees.y
 
-func _input(event):
-	if event is InputEventKey:
-		#turn left
-		if event.keycode == KEY_A:
-			playerInput.y = -1
-		#turn right
-		if event.keycode == KEY_D:
-			playerInput.y = 1
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
 	#get forward vector for rotation
-	forward = Vector2(roundf(position.x), roundf(position.z))
 	nextSpace = Vector2(0,0);
 	
 	if(rotation_degrees.y > 360):
 		rotation_degrees = Vector3(0,rotation_degrees.y -360,0)
 	if(rotation_degrees.y < 0):
 		rotation_degrees = Vector3(0,rotation_degrees.y +360,0)
-	
 	var nearest90 : int = roundf(rotation_degrees.y / 90) * 90
-	
 	match nearest90:
 		0,360:
-			forward.y += 1
-			nextSpace.y = 1
-		90:
-			forward.x += 1
-			nextSpace.x = 1
-		180:
-			forward.y -= 1
 			nextSpace.y = -1
-		270,-90:
-			forward.x -= 1
+		90:
 			nextSpace.x = -1
+		180:
+			nextSpace.y = 1
+		270,-90:
+			nextSpace.x = 1
 	
 	#take time off clocks
 	moveClock -= delta
@@ -70,11 +56,17 @@ func _process(delta: float) -> void:
 	#backwards
 	if Input.is_key_pressed(KEY_S):
 		playerInput.x = -1
+	#right
+	if Input.is_key_pressed(KEY_D):
+		playerInput.y = 1
+	#left
+	if Input.is_key_pressed(KEY_A):
+		playerInput.y = -1
 	
 		
 	#set target position and rotation
 	#forward
-	if(playerInput.x == 1 && moveClock <= 0):
+	if(playerInput.x == 1 && moveClock <= 0 && !sliding):
 		previousPosition = position
 		targetPosition = position
 		targetPosition.x += nextSpace.x
@@ -82,7 +74,7 @@ func _process(delta: float) -> void:
 		moveClock = moveTime
 		lastMove = nextSpace
 	#backwards
-	if(playerInput.x == -1 && moveClock <= 0):
+	if(playerInput.x == -1 && moveClock <= 0 && !sliding):
 		previousPosition = position
 		targetPosition = position
 		targetPosition.x -= nextSpace.x
@@ -103,26 +95,43 @@ func _process(delta: float) -> void:
 	
 	#check collision
 	if(targetPosition != previousPosition):
-		pass
+		var headPosition = targetPosition
+		headPosition.y += 1
+		var blockPos = mazeRef.local_to_map(headPosition)
+		var block = mazeRef.get_cell_item(blockPos)
+		if(block != -1):
+			stopPlayer()
 	
 	#move player
 	if(moveClock >= 0):
-		position = Vector3(moveCurve.sample((moveTime - moveClock)/moveTime) * (previousPosition - targetPosition) + previousPosition)
+		position = Vector3(moveCurve.sample((moveTime - moveClock)/moveTime) * (targetPosition - previousPosition) + previousPosition)
 	#make sure player is at centre of tile when stopped
-	elif(moveClock >= -0.1):
-		position = round(position)
+	elif(moveClock >= -0.5):
+		moveClock = -1
+		stopPlayer()
+		#check ice
+		var blockPos = mazeRef.local_to_map(position)
+		var block = mazeRef.get_cell_item(blockPos)
+		if(block == 1):
+			targetPosition.x += lastMove.x
+			targetPosition.z += lastMove.y
+			moveClock = moveTime
+			sliding = true
 
-	#print("_____________________")
-	#print(previousRotation)
-	#print(rotation)
-	#print(targetRotation)
 
 	#rotate player
 	if(rotationClock >= 0):
 		rotation_degrees = Vector3(0,rotationCurve.sample((rotationTime - rotationClock)/rotationTime) * (previousRotation - targetRotation) + previousRotation,0)
-	elif(rotationClock >= -0.1):
-		pass
+	elif(rotationClock >= -0.5):
+		rotationClock = -1
 		rotation_degrees = Vector3(0,round(rotation_degrees.y / 90) * 90,0)
 		
 	#reset input
 	playerInput = Vector2(0,0)
+	
+func stopPlayer() -> void:
+	position = round(position)
+	targetPosition = position
+	previousPosition = position
+	sliding = false
+	moveClock = -1
