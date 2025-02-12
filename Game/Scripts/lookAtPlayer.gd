c4168a5add3c3a38638457fbad2821345143b231
Adam Anderson <anderada@sheridancollege.ca>
2025-02-12 23:26:33 +0000

    added final maze part

--- Game/Scripts/lookAtPlayer.gd
+++ Game/Scripts/lookAtPlayer.gd
@@ -4,7 +4,8 @@ extends Node3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(%Player.global_position)
-	rotation_degrees.x = 0
-	rotation_degrees.y += offset
-	rotation_degrees.z = 0
+	if(%Player != null):
+		look_at(%Player.global_position)
+		rotation_degrees.x = 0
+		rotation_degrees.y += offset
+		rotation_degrees.z = 0
