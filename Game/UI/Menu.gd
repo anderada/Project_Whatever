65634c71624d1703a246543b9cc8b60c91652425
Lozlant <Lozlant@outlook.com>
2025-03-28 02:25:03 -0400

    add art to the Start Menu

--- Game/UI/Menu.gd
+++ Game/UI/Menu.gd
@@ -3,6 +3,13 @@ extends Node
 @export var mainScene: PackedScene
 @export var OptionsScene: PackedScene
 
+@onready var panel = $"TeamInfo/TeamInfo"   # 获取 UI 节点
+@onready var timer = $"TeamInfo/Timer"   # 获取计时器
+
+func _ready():
+	panel.modulate.a = 0
+	timer.timeout.connect(_hide_panel)
+	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(mainScene)
 
@@ -11,6 +18,12 @@ func _on_option_button_pressed() -> void:
 	var options = load("").instance()
 	get_tree().current_scene.add_child(options)
 
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func _on_team_button_down() -> void:
+	if panel.modulate.a > 0:
+		_hide_panel()
+	else:
+		panel.modulate.a = 1
+		timer.start()
+func _hide_panel():
+	panel.modulate.a = 0
+	timer.stop()
