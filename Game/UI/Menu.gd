496b0787acdecd58ac344da44aeef6d1fb4dc435
Lozlant <Lozlant@outlook.com>
2025-03-28 03:22:31 -0400

    add options func to start menu

--- Game/UI/Menu.gd
+++ Game/UI/Menu.gd
@@ -1,22 +1,25 @@
 extends Node
 
 @export var mainScene: PackedScene
-@export var OptionsScene: PackedScene
 
-@onready var panel = $"TeamInfo/TeamInfo"   # 获取 UI 节点
-@onready var timer = $"TeamInfo/Timer"   # 获取计时器
+@onready var optionsMenu= $OptionsMenu
+@onready var mainMenu= $MainUIElements
+@onready var panel = $MainUIElements/TeamInfo/TeamInfo
+@onready var timer = $MainUIElements/TeamInfo/Timer
 
 func _ready():
 	panel.modulate.a = 0
 	timer.timeout.connect(_hide_panel)
+	optionsMenu.exit_options_menu.connect(_on_exit_options_menu)
 	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(mainScene)
 
 
 func _on_option_button_pressed() -> void:
-	var options = load("").instance()
-	get_tree().current_scene.add_child(options)
+	mainMenu.visible=false
+	optionsMenu.set_process(true)
+	optionsMenu.visible=true
 
 func _on_team_button_down() -> void:
 	if panel.modulate.a > 0:
@@ -27,3 +30,7 @@ func _on_team_button_down() -> void:
 func _hide_panel():
 	panel.modulate.a = 0
 	timer.stop()
+	
+func _on_exit_options_menu() -> void:
+	mainMenu.visible=true
+	optionsMenu.visible=false
