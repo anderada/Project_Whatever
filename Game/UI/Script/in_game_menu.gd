a47178c160e17bab6219d81b063966acbe2b769a
Lozlant <Lozlant@outlook.com>
2025-04-07 14:29:41 -0400

    add an confirm dialouge for back to main menu function

--- Game/UI/Script/in_game_menu.gd
+++ Game/UI/Script/in_game_menu.gd
@@ -2,13 +2,22 @@ extends Control
 
 signal exit_options_menu
 @onready var startMenuScene: PackedScene = load("res://UI/StartMenu.tscn")
+@onready var confirmPanel=$MarginContainer/Confirm_Panel
 
 func _ready() -> void:
 	set_process(false)
 
-func _on_main_menu_button_up() -> void:
+func _on_return_to_main_menu() -> void:
 	get_tree().change_scene_to_packed(startMenuScene)
 
 func _on_back_button_up() -> void:
 	exit_options_menu.emit()
 	set_process(false)
+
+
+func _on_main_menu_button_up() -> void:
+	confirmPanel.visible=true
+
+
+func _on_no_button_up() -> void:
+	confirmPanel.visible=false
