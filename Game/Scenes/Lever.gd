56c61dfcee36358fe3551b1932d65a13c7e13b39
Adam Anderson <anderada@shernet.sheridancollege.ca>
2025-04-07 18:29:44 -0400

    doors animation

--- Game/Scenes/Lever.gd
+++ Game/Scenes/Lever.gd
@@ -1,6 +1,21 @@
 extends Area3D
 
 @export var toMove : GridMap
+@export var greenLight : OmniLight3D
+@export var redLight : OmniLight3D
+@export var animator : AnimationPlayer
+var up : bool = true
 
 func action() -> void:
 	toMove.move()
+	up = !up
+	if(!up):
+		animator.play("LeverAction")
+		greenLight.visible = true
+		redLight.visible = false
+	else:
+		animator.play("LeverAction", -1, -1, true)
+		greenLight.visible = false
+		redLight.visible = true
+	
+	
--- Game/Scripts/telemetry_point.gd
+++ Game/Scripts/telemetry_point.gd
@@ -1,22 +1,23 @@
 extends Node3D
 
-signal start_Stopwatch
+signal trigger
 
+@export var animator : AnimationTree
 var triggered : bool = false
-@export var logMessage = ""
-var stopwatch : float = 0
-var counting : bool = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if(triggered):
 		return
-	if(counting):
-		stopwatch += delta
 	if(abs(%Player.position.distance_to(position)) < 1):
-		Telemetry.log_event("", logMessage, stopwatch if stopwatch != 0 else 0.0)
+		var state_machine = animator["parameters/playback"]
+		if(state_machine != null):
+			state_machine.travel("Open")
+		else:
+			print("no state machine")
+		trigger.emit()
 		triggered = true
-		emit_signal("start_Stopwatch")
+		
 
-func startStopwatch():
-	stopwatch = 0
-	counting = true
+
+func _on_trigger() -> void:
+	triggered = true
